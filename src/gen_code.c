73b37be4f59d35164a8d2a49b2320911a5328035
linguanguo <lin.guanguo2000@gmail.com>
2021-10-20 20:24:20 +0800

    支持多语句re输入，输出nfa/dfa包含接受等级排序

--- src/gen_code.c
+++ src/gen_code.c
@@ -14,29 +14,53 @@
  * 
  * @param state 输入当前状态, 输出下一状态
  * @param c 转移字符
- * @param accept 接受等级 -1代表不接受
+ * @param accept 接受等级 -1: 不接受; 0,1,2...: 接受等级(0级最优先)
  * @return int  -1: 无转移; -2: 未知状态; 0: 进入下一状态;
  */
 int lgg_lex_step(int* state, int c, int* accept){
-    int accept = 0;
     switch (*state){
     case 0:
         switch (c){
-        case 40: *state = 1; *accept = 0; break;
+        case 104: *state = 3; *accept = 2; break;
+        case 49: *state = 2; *accept = -1; break;
+        case 40: *state = 1; *accept = -1; break;
         default: return -1;
         } break;
     case 1:
         switch (c){
-        case 97: *state = 2; *accept = 0; break;
+        case 97: *state = 7; *accept = -1; break;
         default: return -1;
         } break;
     case 2:
         switch (c){
-        case 98: *state = 2; *accept = 0; break;
-        case 41: *state = 3; *accept = 1; break;
+        case 50: *state = 4; *accept = -1; break;
         default: return -1;
         } break;
     case 3:
+        switch (c){
+        default: return -1;
+        } break;
+    case 4:
+        switch (c){
+        case 51: *state = 5; *accept = -1; break;
+        default: return -1;
+        } break;
+    case 5:
+        switch (c){
+        case 53: *state = 6; *accept = 1; break;
+        default: return -1;
+        } break;
+    case 6:
+        switch (c){
+        default: return -1;
+        } break;
+    case 7:
+        switch (c){
+        case 98: *state = 7; *accept = -1; break;
+        case 41: *state = 8; *accept = 0; break;
+        default: return -1;
+        } break;
+    case 8:
         switch (c){
         default: return -1;
         } break;
--- src/main.cc
+++ src/main.cc
@@ -25,11 +25,17 @@ public:
     };
     std::vector<edge_t> edges;    /**< @brief 链式前向星 */
     std::vector<int> head;     /**< @brief 链式前向星 */
+
+    /**
+     * @brief 每个点的接受rank，0为最高，-1为不接受
+     * 
+     */
+    std::vector<int> accept_rank;    
 public:
     /**
      * @brief 遍历一个起点出发的边
      *
-     * @tparam F 遍历函数类型 (edge s))
+     * @tparam F 遍历函数类型 (edge s)
      * @param n 边的起始点
      * @param f 遍历函数
      */
@@ -91,9 +97,40 @@ public:
     {
         if (head.size() <= std::max(start, to)) {
             head.resize(std::max(start, to) + 1, -1);
+            accept_rank.resize(std::max(start, to) + 1, -1);
         }
         edges.push_back({ head[start], to, c });
         head[start] = edges.size() - 1;
+    }
+    
+    /**
+     * @brief 获得点的rank
+     * 
+     * @param i 点序号
+     * @return int rank
+     */
+    int get_rank(int i)
+    {
+        if(head.size() <= i){
+            head.resize(i + 1, -1);
+            accept_rank.resize(i + 1, -1);
+        }
+        return accept_rank[i];
+    }
+
+    /**
+     * @brief 设置点的接受rank
+     * 
+     * @param i 点序号
+     * @param r rank
+     */
+    void set_rank(int i, int r)
+    {
+        if(head.size() <= i){
+            head.resize(i + 1, -1);
+            accept_rank.resize(i + 1, -1);
+        }
+        accept_rank[i] = r;
     }
 
     /**
@@ -102,8 +139,8 @@ public:
      */
     void print()
     {
-        this->visit_all_edge([](int i, edge_t& e) {
-            printf("%d --[%c]-->%d\n", i, e.c == -1 ? '#' : e.c, e.to);
+        this->visit_all_edge([this](int i, edge_t& e) {
+            printf("%d --[%c]-->%d: %d\n", i, e.c == -1 ? '#' : e.c, e.to, this->accept_rank[e.to]);
         });
     }
 };
@@ -115,72 +152,81 @@ public:
 class nfa_t
 {
 public:
+    std::vector<std::string> regexs;
+
     graph_t g;
     int start_node = 0;
-    int end_node;
 
-    nfa_t(std::string re)
+    nfa_t(std::vector<std::string> regexs_) : regexs(regexs_)
     {
         std::stack<std::pair<int, char>> stack;
+        std::vector<int> each_re_start;
         int node_cnt = 0;
-        int lp = 0;
-        for (int i = -1; i <= (int)re.size(); ++i) {
-            char c;
-            // 简化编程实现，整体句子在()中
-            if (i == -1)
-                c = '(';
-            else if (i == re.size())
-                c = ')';
-            else
-                c = re[i];
-
-            if (c == '(') {
-                // 进入括号范围，入栈
-                stack.push({ node_cnt, c });
-            }
-            else if (c == '|') {
-                // |，入栈，并切断与前序的连接(node_cnt++)
-                stack.push({ node_cnt, c });
-                node_cnt++;
-            }
-            else if (c == ')') {
-                // 开始出栈, 清算括号和其中的|
-                std::vector<int> ors;
-                auto top = stack.top(); stack.pop();
-                while (top.second == '|') {
-                    ors.push_back(top.first);
-                    top = stack.top(); stack.pop();
-                }
-                lp = top.first;  // 处理 ( )*
-                for (int or_ : ors) {
-                    // 左括号指向每个|后一个字符，每个|指向结尾
-                    g.add_edge(lp, or_ + 1, g.EPSILON);
-                    g.add_edge(or_, node_cnt, g.EPSILON);
-                }
-            }
-            else if (c == '*') {
-                g.add_edge(lp, node_cnt, g.EPSILON); // 跳过
-                g.add_edge(node_cnt, lp, g.EPSILON); // Loop
-                g.add_edge(node_cnt, node_cnt + 1, g.EPSILON);
-                node_cnt++;
-            }
-            else {
-                if (c == '\\'){
-                    ++i;
+        for(int rank = 0; rank < regexs.size(); ++rank){
+            node_cnt++;
+            each_re_start.push_back(node_cnt);
+            auto& re = regexs[rank];
+            int lp = 0;
+            for (int i = -1; i <= (int)re.size(); ++i) {
+                char c;
+                // 简化编程实现，整体句子在()中
+                if (i == -1)
+                    c = '(';
+                else if (i == re.size())
+                    c = ')';
+                else
                     c = re[i];
+
+                if (c == '(') {
+                    // 进入括号范围，入栈
+                    stack.push({ node_cnt, c });
                 }
-                g.add_edge(node_cnt, node_cnt + 1, c);  // 正常推进
-                lp = node_cnt;  // 处理 c*
-                node_cnt++;
+                else if (c == '|') {
+                    // |，入栈，并切断与前序的连接(node_cnt++)
+                    stack.push({ node_cnt, c });
+                    node_cnt++;
+                }
+                else if (c == ')') {
+                    // 开始出栈, 清算括号和其中的|
+                    std::vector<int> ors;
+                    auto top = stack.top(); stack.pop();
+                    while (top.second == '|') {
+                        ors.push_back(top.first);
+                        top = stack.top(); stack.pop();
+                    }
+                    lp = top.first;  // 处理 ( )*
+                    for (int or_ : ors) {
+                        // 左括号指向每个|后一个字符，每个|指向结尾
+                        g.add_edge(lp, or_ + 1, g.EPSILON);
+                        g.add_edge(or_, node_cnt, g.EPSILON);
+                    }
+                }
+                else if (c == '*') {
+                    g.add_edge(lp, node_cnt, g.EPSILON); // 跳过
+                    g.add_edge(node_cnt, lp, g.EPSILON); // Loop
+                    g.add_edge(node_cnt, node_cnt + 1, g.EPSILON);
+                    node_cnt++;
+                }
+                else {
+                    if (c == '\\'){
+                        ++i;
+                        c = re[i];
+                    }
+                    g.add_edge(node_cnt, node_cnt + 1, c);  // 正常推进
+                    lp = node_cnt;  // 处理 c*
+                    node_cnt++;
+                }
             }
-            end_node = node_cnt;
+            g.set_rank(node_cnt, rank);
+        }
+        for(int i : each_re_start){
+            g.add_edge(this->start_node, i, g.EPSILON);
         }
     }
 
     void print()
     {
         this->g.print();
-        printf("start %d, end %d\n", start_node, end_node);
     }
 };
 
@@ -212,16 +258,17 @@ public:
     std::unordered_map<node_set, int, set_hash> node_group;
 
     int start_node = 0;
-    node_set end_nodes;
 
     /**
      * @brief 从n0出发的epsilon闭包
      *
      * @param n0 出发点
+     * @param highest_rank 转移后闭包中rank最高的点
      * @return node_set epsilon闭包包含的点
      */
-    node_set epsilon_closure(int n0)
+    node_set epsilon_closure(int n0, int* highest_rank)
     {
+        *highest_rank = -1;
         node_set search;
         std::stack<int> no_search;
         no_search.push(n0);
@@ -229,6 +276,13 @@ public:
             int n = no_search.top(); no_search.pop();
             if (search.find(n) == search.end()) {
                 search.insert(n);
+                auto r = this->nfa.g.get_rank(n);
+                if( r != -1) {
+                    if (*highest_rank == -1)
+                        *highest_rank = r;
+                    else
+                        *highest_rank = std::min(*highest_rank, r);
+                }
                 nfa.g.visit_edge_node_c(n, graph_t::EPSILON, [&](graph_t::edge_t& e) {
                     no_search.push(e.to);
                     });
@@ -239,13 +293,15 @@ public:
 
     /**
      * @brief 从n0出发经过c转移后的epsilon闭包
-     *
+     * 
      * @param closure epsilon闭包包含的点
      * @param c 转移输入
+     * @param highest_rank 转移后闭包中rank最高的点
      * @return node_set 转移后的epsilon闭包包含的点
      */
-    node_set epsilon_closure_delta(node_set& closure, int c)
+    node_set epsilon_closure_delta(node_set& closure, int c, int* highest_rank)
     {
+        *highest_rank = -1;
         node_set search;
         std::stack<int> no_search;
         // c转移一步
@@ -260,6 +316,13 @@ public:
             int n = no_search.top(); no_search.pop();
             if (search.find(n) == search.end()) {
                 search.insert(n);
+                auto r = this->nfa.g.get_rank(n);
+                if( r != -1){
+                    if (*highest_rank == -1)
+                        *highest_rank = r;
+                    else
+                        *highest_rank = std::min(*highest_rank, r);
+                }
                 nfa.g.visit_edge_node_c(n, graph_t::EPSILON, [&](graph_t::edge_t& e) {
                     no_search.push(e.to);
                     });
@@ -271,11 +334,11 @@ public:
 
     dfa_t(nfa_t nfa_) : nfa(nfa_)
     {
+        int rank;
         int node_cnt = 0;
-        auto q = epsilon_closure(0);
+        auto q = epsilon_closure(0, &rank);
         node_group.emplace(q, node_cnt);
-        if (q.find(nfa.end_node) != q.end())
-            end_nodes.insert(node_cnt);
+        g.set_rank(node_cnt, rank);
         node_cnt++;
 
         std::stack<node_set> work_list;
@@ -284,7 +347,7 @@ public:
             q = work_list.top(); work_list.pop();
             int from = node_group[q];
             for (int c = 0; c < 255; ++c) {
-                auto t = epsilon_closure_delta(q, c);
+                auto t = epsilon_closure_delta(q, c, &rank);
                 if (t.empty()) 
                     continue;
                 int to;
@@ -292,8 +355,7 @@ public:
                     node_group.emplace(t, node_cnt);
                     work_list.push(t);
                     to = node_cnt;
-                    if (t.find(nfa.end_node) != t.end())
-                        end_nodes.insert(node_cnt);
+                    g.set_rank(node_cnt, rank);
                     node_cnt++;
                 }
                 else {
@@ -307,11 +369,6 @@ public:
     void print()
     {
         this->g.print();
-        printf("start %d, end ", start_node);
-        for(auto i : end_nodes){
-            printf("%d, ", i);
-        }
-        printf("\n");
     }
 
     void gen_code(FILE* file)
@@ -337,7 +394,6 @@ public:
  * @return int  -1: 无转移; -2: 未知状态; 0: 进入下一状态;
  */
 int lgg_lex_step(int* state, int c, int* accept){
-    int accept = 0;
     switch (*state){)";
         constexpr auto state_case = R"(
     case %d:
@@ -357,7 +413,7 @@ int lgg_lex_step(int* state, int c, int* accept){
         for(int n = 0; n < g.head.size(); ++n){
             fprintf(file, state_case, n);
             g.visit_edge_node(n, [&](graph_t::edge_t& e){
-                fprintf(file, char_case, (int)e.c, e.to, (int)(end_nodes.find(e.to) != end_nodes.end()));
+                fprintf(file, char_case, (int)e.c, e.to, this->g.accept_rank[e.to]);
             });
             fprintf(file, char_default);
         }
@@ -365,33 +421,12 @@ int lgg_lex_step(int* state, int c, int* accept){
     }
 };
 
-void test_re2nfa()
-{
-    auto t = nfa_t("ab|cd|ef");
-    t.print();
-    printf("\n\n");
-
-    t = nfa_t("(ab|cd|ef)");
-    t.print();
-    printf("\n\n");
-
-    t = nfa_t("abc*ef");
-    t.print();
-    printf("\n\n");
-
-    t = nfa_t("s(aa|bb)*end");
-    t.print();
-    printf("\n\n");
-
-    t = nfa_t("ab|(cd|cd)*");
-    t.print();
-    printf("\n\n");
-}
-
 void test_dfa()
 {
     {
-        auto n = nfa_t("\\(ab*\\)");
+        auto n = nfa_t({
+            {"\\(ab*\\)"}, {"1235"}, {"h"}
+            });
         n.print();
         printf("\n\n");
         auto d = dfa_t(n);
@@ -402,28 +437,9 @@ void test_dfa()
         d.gen_code(f);
         fclose(f);
     }
-
-    if(0){
-        auto n = nfa_t("int|"
-        "while|"
-        "for|"
-        "do|"
-        "loop|"
-        "((1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*)");
-        n.print();
-        printf("\n\n");
-        auto d = dfa_t(n);
-        d.print();
-        printf("\n\n");
-
-        FILE* f = fopen("gen_code.c", "w");
-        d.gen_code(f);
-        fclose(f);
-    }
 }
 
 int main()
 {
-    //test_re2nfa();
     test_dfa();
 }
